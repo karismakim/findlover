0d9a613b6e996d502552512c426b90677c967168
karismakim <victoryarmy19@gmail.com>
2025-04-11 17:58:16 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🌍 International Love Project</h1>
+      <p>This is a simple React app to test your setup!</p>
+    </div>
+  );
+}
+
+export default App;
